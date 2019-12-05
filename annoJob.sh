153c5e8821082113a3b628ada2543936910e1a18
afollette <afollette@ebi.ac.uk>
2019-12-05 17:13:17 +0000

    Change job running parameters

--- annoJob.sh
+++ annoJob.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#BSUB -J $1_annotater_$(date)
+
 #BSUB --mail-user=afollette@ebi.ac.uk
 #BSUB  -B -N
-#BSUB -e /homes/afollette/$i.err.%j
-#BSUB -o /homes/afollette/$i.out.%j
-#BSUB -M 10000
 #BSUB -n 4
 
 jobName=$1
 
-bsub -J $jobName python2.7 Annotater.py $2 
+bsub -J $jobName -n 4 -M 10000 -R "rusage[mem=10000]" -e /homes/afollette/$i.err.%j -o /homes/afolllette/$i.out.%j python2.7 Annotater.py $2 
 
 
 
